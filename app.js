52bb48cab6b5cfa512c0bd12c8351b1fe6aa87d3
tagawa shouta <sho.webfe@gmail.com>
2022-01-24 20:09:15 +0900

    :+1: first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const { prompt } = require('enquirer');
+const emojis = require('./emojis.json');
+const options = require('./options.json');
+const execSync = require('child_process').execSync;
+
+(async () => {
+  const res = await prompt([
+    {
+      type: 'input',
+      name: 'commitMessage',
+      message: 'コミットメッセージを入力'
+    },
+    {
+      type: 'select',
+      name: 'prefix',
+      message: 'Prefix',
+      choices: options,
+    }
+  ]);
+
+  const index = options.findIndex(option => option.value === res.prefix);
+
+  execSync(`git commit -m ":${emojis[index].emoji}: ${res.commitMessage}"`);
+})()
